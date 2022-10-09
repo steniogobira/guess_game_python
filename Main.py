84497549fc9598f9b3f50966496a970cf0806304
steniogobira <steniogobira@gmail.com>
2022-10-09 16:23:48 -0300

    Melhorias no FrontEnd

--- Main.py
+++ Main.py
@@ -1,18 +1,26 @@
+from cProfile import label
+from email.mime import image
+from tkinter.font import Font
 import pygame
 from random import randrange
 from tkinter import *
 
 janela = Tk()
+
 janela.title('Guess game')
+janela.resizable(False, False)
+janela.wm_attributes('-transparentcolor', 'black')
 
-text1 = Label(janela, text="""
-======================================================
+imagebg = PhotoImage(file="bg.png")
+bg = Label(janela, image=imagebg)
+bg.grid(column=2, row=0)
 
-Olá, vamos brincar?
-Irei pensar em um número de 0 a 10. Tente adivinhar
-
-======================================================
-    """, justify=CENTER)
+frame1 = Frame(janela)
+frame1.grid(column=0, row=0)
+text1 = Label(
+    frame1, text=""" Olá, vamos brincar? 
+    Irei pensar em um número de 0 a 10. 
+    Tente adivinhar""", justify=CENTER, font=("Gotham", "12"))
 text1.grid(column=0, row=0)
 
 
@@ -29,17 +37,18 @@ def layout_game():
     music()
 
     text2 = Label(
-        janela, text="Em qual número eu estou pensando ?", justify=CENTER)
-    text2.grid(column=0, row=3)
+        frame1, text="Em qual número eu estou pensando ?", justify=CENTER, font=("Gotham", "12"))
+    text2.grid(column=0, row=7)
 
     data = StringVar()
-    textbox = Entry(janela, textvariable=data)
-    textbox.grid(column=0, row=4)
-    numero_aleatorio = randrange(11)
+    textbox = Entry(frame1, textvariable=data)
+    textbox.grid(column=0, row=8)
+    numero_aleatorio = 4
+    # randrange(11)
 
-    botão = Button(janela, text='guess', justify=CENTER,
-                   command=lambda: guess(data, numero_aleatorio))
-    botão.grid(column=0, row=5)
+    botão = Button(frame1, text='Guess', justify=CENTER, font=(
+        "Gotham", "13", "bold"), command=lambda: guess(data, numero_aleatorio))
+    botão.grid(column=0, row=10)
 
 
 def guess(data, numero_aleatorio):
@@ -57,21 +66,26 @@ def guess(data, numero_aleatorio):
         Parabéns!! Você acertou. O número que eu estou pensando é {}""".format(numero_aleatorio)
         pygame.mixer.music.fadeout(10000)
         pygame.mixer.music.set_volume(0.2)
-        Texto_resultado = Label(janela, text='', justify=CENTER)
-        Texto_resultado.grid(column=0, row=8)
+        Texto_resultado = Label(
+            frame1, text='', justify=CENTER, font=("Gotham", "12", "bold"))
+        Texto_resultado.grid(column=0, row=12)
         Texto_resultado["text"] = resposta
         return ()
     else:
         pygame.mixer.Sound.play(erro)
         resposta = 'Poxa, tente novamente'
-        Texto_resultado = Label(janela, text='', justify=CENTER)
-        Texto_resultado.grid(column=0, row=8)
+        Texto_resultado = Label(
+            frame1, text='', justify=CENTER, font=("Gotham", "12", "bold"))
+        Texto_resultado.grid(column=0, row=12)
         Texto_resultado["text"] = resposta
         return ()
 
 
-botão = Button(janela, text='Começar!', justify=CENTER, command=layout_game)
-botão.grid(column=0, row=1)
+botão = Button(frame1, text='Jogar', justify=CENTER, font=(
+    "Gotham", "13", "bold"), command=layout_game)
+botão.grid(column=0, row=5)
 
-
+textofinal = Label(janela, text='Desenvolvido por Gobis e Peu', font=(
+    "Gotham", "8", "italic"))
+textofinal.grid(column=2, row=15)
 janela.mainloop()
